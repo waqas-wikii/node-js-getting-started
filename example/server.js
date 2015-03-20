a3a94d76adf80fdac567a037a8bf5ea82fbbef72
waqas <waqas.wikii@gmail.com>
2015-03-20 12:05:43 +0500

    new changes

--- example/server.js
+++ example/server.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var webhook = require('webhook');
+var webhook = require('../lib/webhook');
 
 var app = express();
 
@@ -8,9 +8,9 @@ app.set('port', process.env.PORT || 3000);
 app.use(webhook());
 
 app.get('/', function(req, res) {
-    return res.redirect('https://github.com/b00giZm/express-ifttt-webhook');
+  return res.redirect('https://github.com/b00giZm/express-ifttt-webhook');
 });
 
 var server = app.listen(app.get('port'), function() {
-    console.log('Server listening on port', server.address().port);
+  console.log('Server listening on port', server.address().port);
 });
